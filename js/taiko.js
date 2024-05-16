0e58df1b95f48ff38952650bf6cae7c52c386fb1
yang <caoyang1128@foxmail.com>
2024-05-16 10:33:33 -0700

    add

--- js/taiko.js
+++ js/taiko.js
@@ -392,7 +392,7 @@ function isMobile() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         return true;
     } else {
-        if (window.screen.width < 500){
+        if (window.screen.width < 900){
             return true;
         }
         else{
